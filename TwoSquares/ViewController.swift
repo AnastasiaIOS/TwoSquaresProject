8bb7eb9a972ec25d6ffdbd5980b4831fa6efd4cc
Anastasia <pavlova.a.s@inbox.ru>
2022-02-09 21:54:47 +0300

    Initial Commit

---
+++ TwoSquares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSquares
+//
+//  Created by Anastasia on 09.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
